f7c07bbb64f31a15b022aec613c9dc155290ef75
kshtitijkumrawat20 <kshitijk146@gmail.com>
2025-03-03 15:28:34 +0530

    testing

--- app/models/code_model.py
+++ app/models/code_model.py
@@ -5,8 +5,11 @@ from langchain_core.prompts import ChatPromptTemplate
 class QwenCoderModel:
     def __init__(self):
         self.llm = ChatOllama(model="qwen2.5-coder:0.5b")
+        
 
     def generate_code(self, input: str) -> str:
+        hi = self.llm.invoke("hi")
+        print(hi)
         prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", "You are a helpful assistant which is a pro coder."),
@@ -23,3 +26,5 @@ class QwenCoderModel:
         )
 
         return response  
+    
+    
